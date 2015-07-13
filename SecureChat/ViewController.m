dc90e6801bc8b2ea90d3f47cfe5993d1e2f50a01
Tucker Wang <yangwangbit@gmail.com>
2015-07-12 22:59:49 -0400

    Initial Commit

---
+++ SecureChat/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SecureChat
+//
+//  Created by apple on 2015-07-12.
+//  Copyright (c) 2015 YangTech. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
