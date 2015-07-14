880df92e44fb80bf236def2a8b1566cececad2ab
Tucker Wang <yangwangbit@gmail.com>
2015-07-13 21:45:42 -0400

    First commit for secureChat

---
+++ SecureChat/InboxViewController.h
@@ -0,0 +1,13 @@
+//
+//  InboxViewController.h
+//  SecureChat
+//
+//  Created by apple on 2015-07-12.
+//  Copyright (c) 2015 YangTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InboxViewController : UITableViewController
+
+@end
